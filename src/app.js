572026379e60e632cd705a6efd84ad66cedb02b1
liliasoto <0.lilia.soto.0@gmail.com>
2024-05-26 18:26:30 -0600

    Con permisos para que la use VCWEB

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import productRoutes from './routes/products.routes.js';
 import contactosSoloRoutes from './routes/contactosSolo.routes.js';
+import cors from 'cors';
 
 const app =  express();
+
+// Permitir todas las solicitudes CORS desde http://localhost:5173
+app.use(cors({ origin: 'http://localhost:5173' }));
+
 
 app.use(express.json());
 
